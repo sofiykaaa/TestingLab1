6ce3958c509ef642f944b28da0f2590e056f7913
Igoraaa <sofia25kind@gmail.com>
2024-11-23 16:10:59 +0200

    додано всі файли проекту

---
+++ testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testing")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88cb37ca-6403-4f88-bb9f-33e7699ebdf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testing/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using CalcClassBr;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Рядок підключення для SQL Server
+        string connectionString = "Server=DESKTOP-S63R5EG\\BODYAMSSQL;Database=TESTING;Trusted_Connection=True;";
+
+
+        [TestMethod]
+        public void TestCorrectABSMethod()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT a, expected FROM ABSTestsCorrect", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    long a = Convert.ToInt64(reader["a"]);
+                    int expected = Convert.ToInt32(reader["expected"]);
+
+                    int actual = CalcClass.ABS(a);
+
+                    Assert.AreEqual(expected, actual, "The result is incorrect.");
+                }
+
+                reader.Close();
+            }
+        }
+        [TestMethod]
+        public void TestInCorrectABSMethod()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT a, notexpected FROM ABSTestsIncorrect", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    long a = Convert.ToInt64(reader["a"]);
+                    int notexpected = Convert.ToInt32(reader["notexpected"]);
+
+                    int actual = CalcClass.ABS(a);
+
+                    Assert.AreNotEqual(notexpected, actual, "The result is incorrect.");
+                }
+
+                reader.Close();
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInCorrectArgumentMax()
+        {
+            int actual = CalcClass.ABS((long)int.MaxValue + 1);
+            Assert.AreEqual(0,actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInCorrectArgumentMin()
+        {
+            int actual = CalcClass.ABS((long)int.MinValue - 1);
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
